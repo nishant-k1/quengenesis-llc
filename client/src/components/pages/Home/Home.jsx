5353cc95422826e2ce44597912c8045ddafd0adb
Nishant Kumar <nishant290195.nkr@gmail.com>
2021-02-03 00:13:28 +0530

    redesign home page especially section-2 and footer

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import "./Home.css";
 import React from 'react';
-import heroData,{benefitsData, serviceData, featuresData, testimonialData, bottomData} from './data';
+import section1Data,{section2aData, section2Data, section3Data, section4Data, section6Data} from './data';
 import { Link } from "react-router-dom";
 import SvgComponent from './SvgComponent.jsx';
-import { useTransform, useMotionValue} from "framer-motion";
 
-const HeroSection=(props)=>{
+const Section1=(props)=>{
     return(
         <>
-            <div className="hero">
+            <div className="section-1">
                 <div className='container'>
                     <div className='text-showcase'>
                         <h1>{props.title}</h1>
@@ -24,27 +23,32 @@ const HeroSection=(props)=>{
 }
 
 
-const BenefitsSection=(props)=>{
+const Section2a=(props)=>{
     return( 
-        <div className='benefits'> 
+        <div className='section-2a'> 
             <div className='container'>
-                <div className='benefit-img-showcase'>
-                    <div className="img">
-                        <div className='svg-box'>
-                            <h1>{props.title1}</h1>
-                            <SvgComponent />
-                        </div>
-                        <div className='img-box'>
-                            <h1>{props.title2}</h1>
-                            <img className='benefit-img' src={props.img} alt="img"/>
-                        </div>
-                    </div>
+
+            <div className='text-box'>
+                    <h1>{props.title1}</h1>
+                    <p>{props.desc}</p>
+                </div>
+                <div className='svg-box'>
+                    <SvgComponent />
                 </div>
 
-                <div className='benefit-card-showcase'>
+            </div>
+        </div>
+    );
+}
+
+const Section2=(props)=>{
+    return( 
+        <div className='section-2'> 
+            <div className='container'>
+                <div className='section-2-card-showcase'>
                     {props.cards.map((element, index)=>{
                         return (
-                        <div className='benefit-card' index={index} >
+                        <div className='section-2-card' index={index} >
                             <img className='icon' src={element.img} alt='img'/>
                             <h3>{element.title}</h3>
                             <p>{element.desc}</p>
@@ -57,31 +61,31 @@ const BenefitsSection=(props)=>{
     );
 }
 
-const FeaturesSection=(props)=>{
+const Section3=(props)=>{
     return(
         <>
-        <div className='features'>
-            <div className='container'>
-                <div className='img-div'>
-                    <img className='features-image' src={props.img} alt="feature Img"/>
-                </div>
-                <div className='text-div'>
-                    <h2>{props.desc}</h2>
+            <div className='section-3'>
+                <div className='container'>
+                    <div className='img-div'>
+                        <img className='section-3-image' src={props.img} alt="feature Img"/>
+                    </div>
+                    <div className='text-div'>
+                        <h2>{props.desc}</h2>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
 
-const TestimonialSection=(props)=>{
+const Section4=(props)=>{
     return(
-        <div className='testimony'>
+        <div className='section-4'>
             <div className='container'>
                     {props.cards.map((element, index)=>{
                         return (
-                        <div className='testimony-card' index={index} >
-                            <img className='testimony-img' src={element.img} alt='img'/>
+                        <div className='section-4-card' index={index} >
+                            <img className='section-4-img' src={element.img} alt='img'/>
                             <h2>{element.title}</h2>
                             <p>{element.desc}</p>
                         </div>
@@ -93,10 +97,10 @@ const TestimonialSection=(props)=>{
 }
 
 
-const ServicesSection=(props)=>{
+const Section5=(props)=>{
     return(
         <>
-            <div className='cta'>
+            <div className='section-5'>
                 <div className='container'>
                     <h2>We can transform your future</h2>
                     <Link className='contact-btn' type='button' to='/contact'>Get in touch</Link>
@@ -107,22 +111,15 @@ const ServicesSection=(props)=>{
 }
 
 
-
-const BottomSection=(props)=>{
-    const x = useMotionValue(0)
-    const background = useTransform(
-      x,
-      [-100, 0, 100],
-      ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
-    )
+const Section6=(props)=>{
     return(
-        <div className='bottom-section'>
+        <div className='section-6'>
             <div className='container'>
                 <div className="box">
                     {props.cards.map((element, index)=>{
                         return (
-                            <div className='bottom-card' index={index} >
-                                <img className='bottom-img' src={element.img} alt='img'/>
+                            <div className='section-6-card' index={index} >
+                                <img className='section-6-img' src={element.img} alt='img'/>
                             </div>
                         )
                     })}
@@ -135,12 +132,13 @@ const BottomSection=(props)=>{
 const Home = () => {
     return(
         <>
-            <HeroSection {...heroData} />
-            <BenefitsSection {...benefitsData} />
-            <FeaturesSection {...featuresData} />
-            <TestimonialSection {...testimonialData} />
-            <ServicesSection {...serviceData} />
-            <BottomSection {...bottomData} />
+            <Section1 {...section1Data} />
+            <Section2 {...section2Data} />
+            <Section2a {...section2aData} />
+            <Section3 {...section3Data} />
+            <Section4 {...section4Data} />
+            <Section5 />
+            <Section6 {...section6Data} />
         </>
     );
 }
--- client/src/components/pages/Home/data.js
+++ client/src/components/pages/Home/data.js
@@ -1,84 +1,86 @@
-import featureImg from '../../../assets/featureImg.svg';
-import heroImg from '../../../assets/heroImg.svg';
-import BenefitImg from '../../../assets/benefitImg.svg';
-import BenefitImg1 from '../../../assets/benefitImg1.svg';
-import BenefitImg2 from '../../../assets/benefitImg2.svg';
-import BenefitImg3 from '../../../assets/benefitImg3.svg';
-import BenefitImg4 from '../../../assets/benefitImg4.svg';
-import testimonyImg1 from '../../../assets/testimonyImg1.svg';
-import testimonyImg2 from '../../../assets/testimonyImg2.svg';
-import testimonyImg3 from '../../../assets/testimonyImg3.svg';
-import bottomImg1 from '../../../assets/bottomImg1.svg';
-import bottomImg2 from '../../../assets/bottomImg2.svg';
-import bottomImg3 from '../../../assets/bottomImg3.svg';
-import bottomImg4 from '../../../assets/bottomImg4.svg';
+import section1Img from '../../../assets/section1Img.svg';
+import section2Img from '../../../assets/section2Img.svg';
+import section2Img1 from '../../../assets/section2Img1.svg';
+import section2Img2 from '../../../assets/section2Img2.svg';
+import section2Img3 from '../../../assets/section2Img3.svg';
+import section2Img4 from '../../../assets/section2Img4.svg';
+import section3Img from '../../../assets/section3Img.svg';
+import section4Img1 from '../../../assets/section4Img1.svg';
+import section4Img2 from '../../../assets/section4Img2.svg';
+import section4Img3 from '../../../assets/section4Img3.svg';
+import section6Img1 from '../../../assets/section6Img1.svg';
+import section6Img2 from '../../../assets/section6Img2.svg';
+import section6Img3 from '../../../assets/section6Img3.svg';
+import section6Img4 from '../../../assets/section6Img4.svg';
 
 
-const heroData={
+const section1Data={
     title:"We offer your business a transformative set of solutions outlasting all the current trends.",
     subDesc:"Give us a call",
-    img:heroImg,
+    img:section1Img,
 }
 
-const benefitsData={
-    img: BenefitImg,
-    title1: "We offer global collaboration.",
-    title2: "Engage with us and we can help you reach a new potential.",
+const section2aData={
+    title1: 'Global team of change-makers and thought-leaders.',
+    desc: 'Our team of experienced members and introspective professionals will help you encapsulate new growth opportunities.',
+}
+const section2Data={
+    img: section2Img,
     cards: [    
         {
-            img: BenefitImg1,
+            img: section2Img1,
             title: 'Quality Assurance',
             desc: 'We follow a systematic way of creating an environment to assure that the services being offered meets the quality requirements.',
         },
 
         {
-            img: BenefitImg2,
+            img: section2Img2,
             title: 'Cutting-edge solutions',
             desc: "We take tiime to understand your work practices and the nature of your business to create a custom solution which would turn to be an efficnet soltoutin to your requirements",
         },
 
         {
-            img: BenefitImg3,
+            img: section2Img3,
             title: 'Robust R & D',
             desc: 'We prioritize intense research and analysis before carrying out the any action plan, leading to more sustainable, viable and economical advantages.',
         },
 
-        {
-            img: BenefitImg4,
-            title: 'Global team of change-makers and thought-leaders.',
-            desc: 'Our team of experienced members and introspective professionals will help you encapsulate new growth opportunities.',
-        },
+        // {
+        //     img: section2Img4,
+        //     title: 'Global team of change-makers and thought-leaders.',
+        //     desc: 'Our team of experienced members and introspective professionals will help you encapsulate new growth opportunities.',
+        // },
     ]
 }
  
-const featuresData={
-    img: featureImg,
-    title:'Our Service features',
+const section3Data={
+    img: section3Img,
+    title:'Our Service section3s',
     desc: "We offer global technology and marketing services with an analytical and creative focus at the core of our business engagements.",
 }
 
-const serviceData={
-    title:"Get a free quote now!",
-    desc: 'sdkhjsdhjk  fsdhj hj sdfhjksdfhjkdhjk dhjk',
-}
+// const section4Data={
+//     title:"Get a free quote now!",
+//     desc: 'sdkhjsdhjk  fsdhj hj sdfhjksdfhjkdhjk dhjk',
+// }
 
-const testimonialData={
+const section4Data={
     title: 'This is what our customers say about us',
     cards: 
     [    
         {
-            img: testimonyImg1,
+            img: section4Img1,
             title: 'Technology / IT services',
             desc: 'We work as an extension of your IT team either onsite or off-site offering futuristic and dynamic applications catering to your business requirements.',
         }, 
 
         {
-            img: testimonyImg2,
+            img: section4Img2,
             title: 'API Integration',
             desc: 'We provide API integrations carrying through complex business prerequisites to execution, exhibiting your brands. ',
         },
         {
-            img: testimonyImg3,
+            img: section4Img3,
             title: 'Sales and marketing consultations',
             desc: 'We give a comprehensive analysis of the market and help clients to improve their business. We also assist businesses in selecting and efficiently utilizing the latest marketing tools.',
 
@@ -86,26 +88,26 @@ const testimonialData={
     ]
 }
 
-const bottomData={
+const section6Data={
         cards: 
         [    
             {
-                img: bottomImg1,
+                img: section6Img1,
             }, 
     
             {
-                img: bottomImg2,
+                img: section6Img2,
             },
             {
-                img: bottomImg3,
+                img: section6Img3,
             },
             {
-                img: bottomImg4,
+                img: section6Img4,
             },  
         ]
 }
-export default heroData;
+export default section1Data;
 
-export {benefitsData, serviceData, featuresData, testimonialData, bottomData};
+export {section2aData, section2Data, section3Data, section4Data, section6Data};
 
 
