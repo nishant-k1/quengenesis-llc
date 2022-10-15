b2017a6407b59fffb17cf95b85cf6dcddd064dcb
Nishant Kumar <nishant.29k1@gmail.com>
2022-10-16 00:48:09 +0530

    fix cors issue

--- api/app.js
+++ api/app.js
@@ -1,48 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const bodyParser = require("body-parser");
-var cors = require('cors');
-const { urlencoded } = require('express');
-require('dotenv').config();
-var contactEmails = require('./routes/contactEmails');
-var payments = require('./routes/payments');
+var cors = require("cors");
+const { urlencoded } = require("express");
+require("dotenv").config();
+var contactEmails = require("./routes/contactEmails");
+var payments = require("./routes/payments");
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static( '../client/build'));
-app.use(cors({origin: '/', credentials: true,}));
-app.use('/contactEmails', contactEmails);
-app.use('/payments', payments);
+app.use(express.static("../client/build"));
+app.use(cors());
+app.use("/contactEmails", contactEmails);
+app.use("/payments", payments);
 
-
-app.get('*', (req, res )=>{
-  res.sendFile(path.resolve('..', 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("..", "client", "build", "index.html"));
 });
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json('error');
+  res.json("error");
 });
-
 
 module.exports = app;
